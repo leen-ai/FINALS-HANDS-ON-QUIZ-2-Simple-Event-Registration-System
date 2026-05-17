42bfb30167dad9f385acfb2fb850531ed9fe2e7c
leen-ai <tabanaoleeann@gmail.com>
2026-05-18 03:24:51 +0800

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from events import views
 
+app_name = 'events'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls', namespace='events')),  
