02699298cc0503fda01ee6a56d75ae3bb2f2dfaf
leen-ai <tabanaoleeann@gmail.com>
2026-05-18 03:33:46 +0800

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from events import views
 
+app_name = 'events'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('success/', views.registration_success, name='registration_success'),  
-    path('', views.register_event, name='home'), 
+    path('', views.register_event, name='register_event'),
+    path('success/', views.registration_success, name='registration_success'),   
     ]
